4d05c285a334f875fb3976bc23f0bcb2503957b2
Sunil <to.sunilc@gmail.com>
2022-01-04 19:36:59 +0530

    login proj upto 040122

---
+++ personal/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+
+def home_screen_view(request):
+	context = {}
+	return render(request, "personal/home2.html", context)
+
+def temp(request):
+	context = {}
+	return render(request, "personal/temp.html", context)
+
+
+
+def dashboard(request):
+	context = {}
+	return render(request, "personal/dashboard.html", context)
+
+def home_proj(request):
+	context = {}
+	return render(request, "personal/home.html", context)
